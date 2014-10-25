d53c439263b0454af901dcb884a196dae875e81d
aroblu94 <aroblu94@gmail.com>
2014-10-25 18:20:19 +0200

    corrected figure.c

--- figure.c
+++ figure.c
@@ -2,28 +2,33 @@
 #include <math.h>
 
 typedef struct {
-	int x;
-	int y;
+	int x, y;
 } Punto;
 
 typedef struct {
-	Punto a;
-	Punto b;
+	Punto p1, p2;
 } Rettangolo;
 
 typedef struct {
 	Punto o;
-	int c;
+	double r;
 } Cerchio;
 
 int main () {
 	float areaRett, perimRett, areaCerchio, circCerchio;
-	Rettangolo r = {{0,0}, {4,4}};
-	Cerchio c = {{0,0}, 3};
-	areaRett = abs(0 - 4) * abs(0 - 4);
-	perimRett = abs(0 - 4)*2 + abs(0 - 4)*2;
-	areaCerchio = M_PI * 3 * 3;
-	circCerchio = 2 * 3 * M_PI;
+	Rettangolo r;
+	r.p1.x = 0;
+	r.p1.y = 1;
+	r.p2.x = 5;
+	r.p2.y = 3;
+	Cerchio c;
+	c.o.x = 0;
+	c.o.y = 1;
+	c.r = 3;
+	areaRett = abs(r.p1.x - r.p2.x) * abs(r.p1.y - r.p2.y);
+	perimRett = abs(r.p1.x - r.p2.x)*2 + abs(r.p1.y - r.p2.y)*2;
+	areaCerchio = M_PI * pow(c.r, 2);
+	circCerchio = 2 * c.r * M_PI;
 	printf("Area rettangolo: %f\nPerimetro rettangolo: %f\nArea cerchio: %f\nCirconferenza cerchio: %f\n", areaRett, perimRett, areaCerchio, circCerchio);
 	return 0;
 }
