64d5971118a9d32a502ed7602d637a3d72b61957
aroblu94 <aroblu94@gmail.com>
2014-11-03 17:49:07 +0100

    Added farf.c

---
+++ farf.c
@@ -0,0 +1,25 @@
+// Esercizio 2.1
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char **argv) {
+	char *word;
+	word = argv[1];
+	while(*word != '\0') {
+		if(*word == 'a')
+			printf("afa");
+		else if(*word == 'e')
+			printf("efe");
+		else if(*word == 'i')
+			printf("ifi");
+		else if(*word == 'o')
+			printf("ofo");
+		else if(*word == 'u')
+			printf("ufu");
+		else
+			printf("%c", *word);
+		word++;
+	}
+	printf("\n");
+	return 0;
+}
