88fd9bc80bf5aeb9c69c04813f1ed93ff9647565
Aronne Brivio <aroblu94@gmail.com>
2015-10-09 09:13:13 +0200

    Update Lezione 1

--- Farenheit-Celsius.c
+++ Farenheit-Celsius.c
@@ -2,12 +2,10 @@
 #include <stdio.h>
 
 int main() {
-    float d=0;
-    printf("Inserisci la temperatura in Farenheit\n");
-    scanf("%f", &d);
-    int d1=0;
-    d1=d-32;
-    d=d1/1.8;
-    printf("Temperatura in Celsius: %f\n", d);
-    return 0;
+	float temp=0;
+	printf("Farenheit: \n");
+	scanf("%f", &temp);
+	temp=(temp-32)/1.8;
+	printf("Celsius: %f", temp);
+	return 0;
 }
